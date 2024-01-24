15a632c9a1fa7f4a9c0a6acb832d151fcd15736c
Shubhendu Ram Tripathi <shubhendu@minio.io>
2024-01-24 14:19:51 +0530

    With s3 and chunked encoding
    Signed-off-by: Shubhendu Ram Tripathi <shubhendu@minio.io>

--- src/main/java/org/example/DependencyFactory.java
+++ src/main/java/org/example/DependencyFactory.java
@@ -3,6 +3,7 @@ package org.example;
 
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
@@ -17,9 +18,12 @@ public class DependencyFactory {
      * @return an instance of S3Client
      */
     public static S3AsyncClient s3Client() {
+        S3Configuration config = S3Configuration.builder().chunkedEncodingEnabled(true).build();
         return S3AsyncClient.builder()
-                .endpointOverride(URI.create("http://127.0.0.1:9000"))
+//                .endpointOverride(URI.create("http://127.0.0.1:9000"))
+                .endpointOverride(URI.create("https://s3.amazonaws.com"))
                 .credentialsProvider(ProfileCredentialsProvider.create())
+                .serviceConfiguration(config)
                 .build();
     }
 }
